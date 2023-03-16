7c2dc1f4c19efcc896bd24e037b2157f93545536
Phong Hoang <p.hoang6900@gmail.com>
2023-03-16 11:31:19 -0400

    Save for merge and pull

--- client/src/components/Student.vue
+++ client/src/components/Student.vue
@@ -2,8 +2,22 @@
   <div></div>
 </template>
 <script>
+// import axios from 'axios'
+
 export default {
   // eslint-disable-next-line
-  name: 'Student'
+  name: 'Student',
+  data: () => {
+    // student: []
+  },
+  methods: {
+    // async getAllStudents() {
+    //   const response = await axios.get()
+    //   this.student = response
+    // },
+    // async getStudentById() {
+    //   const response = await axios.get()
+    // }
+  }
 }
 </script>
