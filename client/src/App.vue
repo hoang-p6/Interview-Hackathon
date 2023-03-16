34b90beb37218882b9895b2e0e83f95640950977
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-16 13:02:40 -0500

    commented out hompage component from App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <NavBar />
   <div>
-    <HomePage />
+    <!-- <HomePage /> -->
     <router-view header="all"></router-view>
   </div>
 </template>
 
 <script>
-import HomePage from "./components/HomePage.vue";
+// import HomePage from "./components/HomePage.vue";
 import NavBar from "./components/NavBar.vue";
 export default {
   name: "App",
-  components: { HomePage, NavBar },
+  components: { NavBar },
 };
 </script>
 
