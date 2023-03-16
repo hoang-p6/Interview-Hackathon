c7947120d031a2b32b2e8b13eb5dcc18461beaf7
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-16 13:26:41 -0500

    added placeholder information

--- client/src/components/StudentCard.vue
+++ client/src/components/StudentCard.vue
@@ -1,15 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <h3>Hector Flores</h3>
+      <h3>GPA: 3</h3>
+      <h4>hmflores88@student.edu</h4>
+    </div>
+    <h2>Courses:</h2>
+    <div>
+      <p>AWE 111</p>
+      <p>Grade: A</p>
+    </div>
+  </div>
 </template>
 <script>
 // import axios from 'axios'
 
 export default {
   // eslint-disable-next-line
-  name: 'Student',
-  data: () => {
-    // student: []
-  },
+  name: "Student",
+  // data: () => {
+  //   // student: []
+  // },
   methods: {
     // async getAllStudents() {
     //   const response = await axios.get()
@@ -18,6 +29,6 @@ export default {
     // async getStudentById() {
     //   const response = await axios.get()
     // }
-  }
-}
+  },
+};
 </script>
