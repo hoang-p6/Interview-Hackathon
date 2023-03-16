a16c998fb9ebb89cde10bff8950c35d9f934126f
mannyaalonso <mannyaalonso@gmail.com>
2023-03-16 14:22:33 -0400

    migrations

---
+++ migrations/20230316181545-students.js
@@ -0,0 +1,41 @@
+"use strict"
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("students", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("students")
+  },
+}
