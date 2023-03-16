e757da8ece27ec405823f4bfe4aed465f7f5354b
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-16 09:54:15 -0500

    initial routes in NavBar created

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,10 +1,15 @@
 <template>
-  <div></div>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/courses">Courses</router-link>
+    <router-link to="/students">Students</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
 </template>
 <script>
 // import StudentForm from './StudentForm.vue'
 export default {
-  name: 'NavBar',
-  components: {}
-}
+  name: "NavBar",
+  components: {},
+};
 </script>
